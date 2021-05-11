8286cfb6bfeb289f807736ed4d4df61e00e9bcdb
Rafael Dias <rafaeldias@Rafael-s-Macbook-Pro.local>
2021-05-11 20:04:13 +0100

    newer version of morseTranslator, using an array of structs however that required another for loop which is kinda yikes but hey, looks cleaner and if i'm still motivated some time later i'll have it translate morse characters into latin ones

---
+++ cmake-build-debug/CMakeFiles/morseTranslator2.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/morseTranslator2.dir/main.c.o"
+  "morseTranslator2"
+  "morseTranslator2.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/morseTranslator2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
